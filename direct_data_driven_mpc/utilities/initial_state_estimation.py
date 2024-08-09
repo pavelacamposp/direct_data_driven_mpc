6445ea1a4c813bacf59524a84f3492ec77413ea4
Pável A. Campos-Peña <axelmccampospena@gmail.com>
2024-08-08 23:34:20 -0500

    Add initial state estimation utility script to 'utilities'
    Add 'initial_state_estimation.py' to 'utilities' directory. This
    module provides functions used for estimating the initial state
    of a system.

---
+++ direct_data_driven_mpc/utilities/initial_state_estimation.py
@@ -0,0 +1,94 @@
+import numpy as np
+
+def observability_matrix(A: np.ndarray, C: np.ndarray) -> np.ndarray:
+    """
+    Calculate the observability matrix for a state-space system defined by
+    state matrix A and output matrix C.
+
+    The observability matrix is constructed over n time-steps, where n is the
+    number of states in the system, which corresponds to the dimension of the
+    A matrix.
+
+    Args:
+        A (np.ndarray): The State matrix.
+        C (np.ndarray): The Output matrix.
+    
+    Returns:
+        np.ndarray: The observability matrix of the system.
+    """
+    # Get number of states
+    n = A.shape[0] # Number of states
+
+    Ot = np.vstack([C @ np.linalg.matrix_power(A, i) for i in range(n)])
+    
+    return Ot
+
+def toeplitz_input_output_matrix(
+    A: np.ndarray,
+    B: np.ndarray,
+    C: np.ndarray,
+    D: np.ndarray,
+    t: int
+) -> np.ndarray:
+    """
+    Construct a Toeplitz matrix that maps inputs to outputs for a state-space
+    system defined by matrices A (state), B (input), C (output) and D
+    (feedforward), over the time interval [0, t-1].
+
+    This matrix is used to express the linear response of the system outputs
+    to the system inputs, extended over t time steps.
+
+    For t = 3, this matrix takes the form:
+    Tt = [[D   0   0],
+          [CB  D   0],
+          [CAB CB  D]]
+    
+    Args:
+        A (np.ndarray): The State matrix.
+        B (np.ndarray): The Input matrix.
+        C (np.ndarray): The Output matrix.
+        D (np.ndarray): The Feedforward matrix.
+        t (int): The number of time steps for the Toeplitz matrix extension.
+    
+    Returns:
+        np.ndarray: The Toeplitz input-output matrix of the system over t
+            steps.
+            
+    Examples:
+        >>> import numpy as np
+        >>> A = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        >>> B = np.array([[1], [1], [0]])
+        >>> C = np.array([[1, 0, 2], [0, 1, 0]])
+        >>> D = np.array([[0], [1]])
+        >>> t = 3
+        >>> print(toeplitz_input_output_matrix(A, B, C, D, t))
+        [[ 0.  0.  0.]
+         [ 1.  0.  0.]
+         [ 1.  0.  0.]
+         [ 1.  1.  0.]
+         [33.  1.  0.]
+         [ 9.  1.  1.]]
+    """
+    if t <= 0:
+        raise ValueError("The number of time steps t must be positive.")
+
+    # Get number of inputs and outputs
+    m = B.shape[1] # Number of inputs
+    p = C.shape[0] # Number of outputs
+
+    # Precompute powers of A
+    A_pows = [np.linalg.matrix_power(A, i) for i in range(t)]
+
+    # Construct Toeplitz input-output matrix
+    Tt = np.zeros((p * t, m * t))
+    for i in range(t):
+        for j in range(t):
+            if i == j:
+                Tt[i * p: (i + 1) * p,
+                   j * m: (j + 1) * m] = D
+            elif j < i:
+                Tt[i * p: (i + 1) * p,
+                   j * m: (j + 1) * m] = C @ A_pows[i - j - 1] @ B
+    
+    return Tt
+
