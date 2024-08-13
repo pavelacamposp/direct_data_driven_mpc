9bbd93024de3cee00c03e80bc478381d8b558c1b
Pável A. Campos-Peña <axelmccampospena@gmail.com>
2024-08-13 05:54:58 -0500

    Add 'setup.py' for package installation and distribution

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="direct_data_driven_mpc",
     version='1.0',
-    description=("A Python implementation of a Nominal and Robust Direct"
-                 "Data-Driven MPC Control system proposed by "),
+    description=("A Python implementation of Nominal and Robust Direct "
+                 "Data-Driven MPC Controllers proposed by Julian Berberich, "
+                 "Johannes Köhler, Matthias A. Müller, and Frank Allgöwer."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Pável A. Campos-Peña',
     author_email='pcamposp@uni.pe',
+    url='https://github.com/pavelacamposp/direct-data-driven-mpc',
     packages=setuptools.find_packages(include=["direct_data_driven_mpc*",
                                                "models*"]),
     install_requires=['numpy', 'matplotlib', 'cvxpy'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     license='MIT'
 )
