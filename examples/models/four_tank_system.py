961941711fb5bcb0b16d34f8cb3bad98c027cf65
Pável A. Campos-Peña <pcamposp@uni.pe>
2024-09-10 18:34:14 -0500

    Add `FourTankSystem` class to modularize example model simulation
    - Implement a `FourTankSystem` class in 'four_tank_system.py' to
    define the example four-tank system model and modularize its
    simulation.
    - Remove the `FourTankSysParams` class, as the model parameters are
    now defined in a YAML config file ('four_tank_system_params.yaml').
    
    This change modularizes the example model simulations, improving
    the overall code structure and maintainability.

--- examples/models/four_tank_system.py
+++ examples/models/four_tank_system.py
@@ -1,47 +1,68 @@
 import numpy as np
+import os
 
-class FourTankSysParams:
+from utilities.yaml_config_loading import load_yaml_config_params
+
+from utilities.model_simulation import LTIModel
+
+# Directory paths
+dirname = os.path.dirname
+models_directory = dirname(__file__)
+models_config_directory = os.path.join(models_directory, 'config')
+
+# Model config file paths
+model_config_file = 'four_tank_system_params.yaml'
+model_config_path = os.path.join(models_config_directory, model_config_file)
+
+# Model config key
+model_key_value = 'FourTankSystem'
+
+class FourTankSystem(LTIModel):
     """
-    Parameters of the linearized version of a four tank system considered in
-    J. Berberich, J. Köhler, M. A. Müller and F. Allgöwer, "Data-Driven Model
-    Predictive Control With Stability and Robustness Guarantees," in IEEE
-    Transactions on Automatic Control, vol. 66, no. 4, pp. 1702-1717, April
-    2021, doi: 10.1109/TAC.2020.3000182.
-
-    This class encapsulates the system matrices (A, B, C, D) used in the
-    linearized state-space representation of the system, and the control
-    setpoints (us, ys).
-
-    The linearized model is given by:
-        x(k+1) = A * x(k) + B * u(k)
-        y(k) = C * x(k) + D * u(k)
-    
-    The control goal of the system, as defined in the paper, is given by
-    (us, ys).
+    A class that defines the model of a linearized version of a four-tank
+    system.
 
     Attributes:
         A (np.ndarray): System state matrix.
         B (np.ndarray): Input matrix.
         C (np.ndarray): Output matrix.
         D (np.ndarray): Feedforward matrix.
-        us (np.ndarray): Setpoint for control input.
-        ys (np.ndarray): Setpoint for output.
+        eps_max (float): Upper bound of the system measurement noise.
+
+    Note:
+        The model is based on the linearized version of the four-tank system
+        from the example implementation in Section V from [1].
+    
+    References:
+        [1] J. Berberich, J. Köhler, M. A. Müller and F. Allgöwer,
+            "Data-Driven Model Predictive Control With Stability and
+            Robustness Guarantees," in IEEE Transactions on Automatic Control,
+            vol. 66, no. 4, pp. 1702-1717, April 2021,
+            doi: 10.1109/TAC.2020.3000182.
     """
-    A = np.array([[0.921, 0, 0.041, 0],
-                  [0, 0.918, 0, 0.033],
-                  [0, 0, 0.924, 0],
-                  [0, 0, 0, 0.937]], dtype=float)
+    def __init__(self, verbose: int = 0):
+        """
+        Initialize a Four-Tank system model by loading parameters from a
+        predefined YAML configuration file.
 
-    B = np.array([[0.017, 0.001],
-                  [0.001, 0.023],
-                  [0, 0.061],
-                  [0.072, 0]], dtype=float)
+        Args:
+            verbose (int): The verbose level.
+        """
+        self.verbose = verbose
+        
+        # Load model parameters from config file
+        params = load_yaml_config_params(config_file=model_config_path,
+                                         key=model_key_value)
 
-    C = np.array([[1, 0, 0, 0],
-                  [0, 1, 0, 0]], dtype=float)
-
-    D = np.zeros((2, 2), dtype=float)
-
-    # Control goal parameters
-    us = np.array([1, 1], dtype=float)
-    ys = np.array([0.65, 0.77], dtype=float)
+        if self.verbose:
+            print(f"Loaded model parameters from {model_config_path}")
+        
+        # Get model parameters
+        A = np.array(params['A'], dtype=float)
+        B = np.array(params['B'], dtype=float)
+        C = np.array(params['C'], dtype=float)
+        D = np.array(params['D'], dtype=float)
+        eps_max = params['eps_max']
+        
+        # Initialize the base LTIModel class with the loaded parameters
+        super().__init__(A=A, B=B, C=C, D=D, eps_max=eps_max)
